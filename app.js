1fa5894d9e46662b10bf8ebfb0bd462314228d77
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-22 13:36:33 -0700

    finished render-utils.js, time to tie it all together in app.js

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // import functions and grab DOM elements
-
+import { getAllBands } from './fetch-utils.js';
+import { renderBands } from './render-utils.js';
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const bandsListEl = document.querySelector('.bands');
+
+window.addEventListener('load', async () => {
+    fetchAndDisplayBands();
+    fetchAndDisplayCandies();
+    fetchAndDisplayCars();
+    fetchAndDisplayInstruments();
+});
+
+async function fetchAndDisplayBands() {
+    const bands = await getAllBands();
+    for (let band of bands) {
+        const bandEl = renderBands(band);
+        bandsListEl.append(bandEl);
+    }
+}
---
+++ render-utils.js
@@ -0,0 +1,102 @@
+export function renderBands(bands) {
+    const bandEl = document.createElement('div');
+    const namesEl = document.createElement('h2');
+    const genreEl = document.createElement('p');
+    const subgenreEl = document.createElement('p');
+    const foundedEl = document.createElement('p');
+
+    bandEl.classList.add('bands');
+    
+    namesEl.textContent = bands.name;
+    genreEl.textContent = bands.genre;
+    subgenreEl.textContent = bands.subgenre;
+    foundedEl.textContent = bands.year_founded;
+
+    bandEl.append(
+        namesEl,
+        genreEl,
+        subgenreEl,
+        foundedEl
+    );
+
+    return bandEl;
+}
+
+export function renderCandies(candies) {
+    const candyEl = document.createElement('div');
+    const namesEl = document.createElement('h2');
+    const brandEl = document.createElement('p');
+    const categoryEl = document.createElement('ul');
+    const yearEl = document.createElement('p');
+
+    candyEl.classList.add('bands');
+    
+    namesEl.textContent = candies.name;
+    brandEl.textContent = candies.brand;
+    yearEl.textContent = candies.year;
+
+    for (let category of candies.category) {
+        const li = document.createElement('li');
+        li.textContent = category;
+        categoryEl.appendChild(li);
+    }
+
+    candyEl.append(
+        namesEl,
+        brandEl,
+        categoryEl,
+        yearEl
+    );
+
+    return candyEl;
+}
+
+export function renderCars(cars) {
+    const carEl = document.createElement('div');
+    const brandEl = document.createElement('h2');
+    const typeEl = document.createElement('h2');
+    const yearEl = document.createElement('p');
+    const interiorEl = document.createElement('ul');
+
+    carEl.classList.add('bands');
+    
+    brandEl.textContent = cars.brand;
+    typeEl.textContent = cars.type;
+    yearEl.textContent = cars.year;
+
+    for (let interior of cars.interior) {
+        const li = document.createElement('li');
+        li.textContent = interior;
+        interiorEl.appendChild(li);
+    }
+
+    carEl.append(
+        brandEl,
+        typeEl,
+        yearEl,
+        interiorEl
+    );
+
+    return carEl;
+}
+
+export function renderInstruments(instruments) {
+    const instrumentEl = document.createElement('div');
+    const namesEl = document.createElement('h2');
+    const yearEl = document.createElement('p');
+    const stringCountEl = document.createElement('p');
+
+    instrumentEl.classList.add('bands');
+    
+    namesEl.textContent = instruments.name;
+    yearEl.textContent = instruments.year_invented;
+    stringCountEl.textContent = `This instrument has ${instruments.string_count} strings!`;
+
+    instrumentEl.append(
+        namesEl,
+        yearEl,
+        stringCountEl,
+    );
+
+    return instrumentEl;
+}
